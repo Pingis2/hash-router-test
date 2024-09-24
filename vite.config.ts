421f07a7f15cdd1897eaf0204ff788531c47fe03
Pingis2 <anton@worksit.se>
2024-09-24 12:24:48 +0200

    updated base route

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/https://github.com/Pingis2/hash-router-test/",
+  base: "/Pingis2/hash-router-test/",
   plugins: [react()],
 })
