46bba242e52fd77059429b2fe51d68ee3ea529da
Pingis2 <anton@worksit.se>
2024-09-24 12:48:45 +0200

    added text

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,11 @@ function App() {
           <ul>
             <li>
               <Link to="/">Home</Link>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores optio sit molestiae expedita, nisi aut nobis cum ducimus praesentium consequuntur accusamus, fugiat ad iste, reiciendis cumque nulla esse veniam animi.</p>
             </li>
             <li>
               <Link to="/contacts">Contacts</Link>
+              <p>bajs bajs bajs bajs bajs bajsbaajasjd</p>
             </li>
           </ul>
         </nav>
